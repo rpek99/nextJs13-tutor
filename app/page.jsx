e6b9af07345180f570a52b627b3d70c9ed409ef8
rpek99 <rstmpk28@gmail.com>
2023-05-12 15:39:38 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import Feed from "@components/Feed"
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">
+                AI-Powered Prompts
+            </span>
+        </h1>
+        <p className="desc text-center">
+            Lorem ipsum dolor sit amet consectetur elit.
+            Distinctio sequi debitis voluptatibus odio
+            deserunt nihil unde facere eligendi.
+        </p>
+
+        <Feed />
+    </section>
+  )
+}
+export default Home
+
+
